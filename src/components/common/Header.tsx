edeb7eb1b27faec923886a595ab535e3bee28c3a
Matthew Gilligan <mcgilligan97@gmail.com>
2023-12-01 16:40:16 +0900

    style onauth header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -58,24 +58,20 @@ export default function Header() {
           </ul>
 
           <div className="flex items-center lg:order-2">
+            <div className="flex justify-between border border-gray-300 rounded-full py-2 px-4 mr-6">
+              <div className="flex">
+                <IconContext.Provider value={{ color: "black", size: "1.4em" }}>
+                  <div onClick={() => router.push("/sign-in")} className="mr-2">
+                    <LuSearch />
+                  </div>
+                </IconContext.Provider>
+                <p className="pr-20">Search</p>
+              </div>
+              <div className="border-l pl-3">Work</div>
+            </div>
+
             {user && (
               <>
-                <div className="flex justify-between border border-gray-300 rounded-full py-2 px-4 mr-6">
-                  <div className="flex">
-                    <IconContext.Provider
-                      value={{ color: "black", size: "1.4em" }}
-                    >
-                      <div
-                        onClick={() => router.push("/sign-in")}
-                        className="mr-2"
-                      >
-                        <LuSearch />
-                      </div>
-                    </IconContext.Provider>
-                    <p className="pr-20">Search</p>
-                  </div>
-                  <div className="border-l pl-3">Work</div>
-                </div>
                 <IconContext.Provider value={{ color: "black", size: "1.7em" }}>
                   <div onClick={() => router.push("/sign-in")} className="mr-6">
                     <IoPaperPlaneOutline />
@@ -94,52 +90,18 @@ export default function Header() {
               <>
                 <p
                   onClick={() => router.push("/sign-in")}
-                  className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+                  className="text-gray-800 dark:text-white hover:text-orange focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-md px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 cursor-pointer focus:outline-none dark:focus:ring-gray-800"
                 >
                   Log in
                 </p>
                 <p
                   onClick={() => router.push("/register")}
-                  className="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800"
+                  className="text-white bg-orange hover:opacity-90 focus:ring-4 focus:ring-primary-300 font-medium rounded-full text-md px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 cursor-pointer focus:outline-none dark:focus:ring-primary-800"
                 >
                   Get started
                 </p>
               </>
             )}
-            <button
-              data-collapse-toggle="mobile-menu-2"
-              type="button"
-              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-              aria-controls="mobile-menu-2"
-              aria-expanded="false"
-              onClick={() => router.push("/")}
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-              <svg
-                className="hidden w-6 h-6"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </button>
           </div>
         </div>
       </nav>
