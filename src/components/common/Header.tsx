b6ff245abac3bc8a8291d6b39b4752a39435cc63
Matthew Gilligan <mcgilligan97@gmail.com>
2023-12-01 15:02:40 +0900

    Add icons to header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -3,6 +3,9 @@ import { auth } from "@/lib/firebase/config";
 import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 import AvatarMenu from "./AvatarMenu";
+import { IoPaperPlaneOutline } from "react-icons/io5";
+import { GoBell } from "react-icons/go";
+import { IconContext } from "react-icons";
 
 export default function Header() {
   const router = useRouter();
@@ -56,18 +59,17 @@ export default function Header() {
           <div className="flex items-center lg:order-2">
             {user && (
               <>
-                <p
-                  onClick={() => router.push("/sign-in")}
-                  className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-                >
-                  Messaging
-                </p>
-                <p
-                  onClick={() => router.push("/sign-in")}
-                  className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-                >
-                  Notifications
-                </p>
+                <IconContext.Provider value={{ color: "black", size: "1.7em" }}>
+                  <div onClick={() => router.push("/sign-in")} className="mr-6">
+                    <IoPaperPlaneOutline />
+                  </div>
+                </IconContext.Provider>
+                <IconContext.Provider value={{ color: "black", size: "1.8em" }}>
+                  <div onClick={() => router.push("/sign-in")} className="mr-2">
+                    <GoBell />
+                  </div>
+                </IconContext.Provider>
+
                 <AvatarMenu />
               </>
             )}
