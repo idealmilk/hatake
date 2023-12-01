12f212ad28f33fe22ea0e50c2dd33cd375fbf63a
Matthew Gilligan <mcgilligan97@gmail.com>
2023-12-01 16:30:00 +0900

    Add style to auth pages

--- src/components/RegisterComponent.tsx
+++ src/components/RegisterComponent.tsx
@@ -18,8 +18,8 @@ export default function RegisterComponent() {
       let res = await SignUpAPI(credentials);
       toast.success("Account created");
       router.push("/");
-    } catch (err: any) {
-      console.log(err);
+    } catch (err) {
+      console.error(err);
       toast.error("Cannot create your account");
     }
   };
@@ -29,48 +29,105 @@ export default function RegisterComponent() {
       let res = await GoogleSignInAPI();
       toast.success("Signed in to LinkedIn");
       router.push("/");
-    } catch (err: any) {
-      console.log(err);
+    } catch (err) {
+      console.error(err);
     }
   };
 
   return (
-    <div>
-      <h1>Register Component</h1>
-      <p>Make the most of your professional life</p>
-      <div>
-        <input
-          className="block"
-          onChange={(event) => {
-            setCredentials({ ...credentials, displayName: event.target.value });
-          }}
-          placeholder="Your Name"
-          type="text"
-        />
-        <input
-          className="block"
-          onChange={(event) => {
-            setCredentials({ ...credentials, email: event.target.value });
-          }}
-          placeholder="Email or Phone"
-          type="email"
-        />
-        <input
-          className="block"
-          onChange={(event) => {
-            setCredentials({ ...credentials, password: event.target.value });
-          }}
-          placeholder="Password (6 or more characters)"
-          type="password"
-        />
-        <button className="block" onClick={signUp}>
-          Agree & Join
-        </button>
+    <div className="h-screen bg-white relative flex flex-col space-y-6 justify-center items-center">
+      <div
+        onClick={() => router.push("/")}
+        className="absolute  top-3 left-5 cursor-pointer"
+      >
+        <span className="self-center text-3xl font-semibold whitespace-nowrap text-orange dark:text-white">
+          hatake
+        </span>
       </div>
-      <GoogleButton onClick={signInWithGoogle} />
+      <div className="bg-white md:shadow-lg shadow-none rounded p-6 w-96">
+        <h1 className="text-3xl font-bold leading-normal">
+          Register Component
+        </h1>
+        <p className="text-sm leading-normal">
+          Make the most of your professional life
+        </p>
+        <form className="space-y-5 mt-5">
+          <div className="mb-4 relative">
+            <input
+              className="w-full rounded px-3 border border-gray-500 pt-5 pb-2 focus:outline-none input active:outline-none"
+              onChange={(event) => {
+                setCredentials({
+                  ...credentials,
+                  displayName: event.target.value,
+                });
+              }}
+              placeholder="Your Name"
+              type="text"
+            />
+          </div>
+
+          <div className="mb-4 relative">
+            <input
+              className="w-full rounded px-3 border border-gray-500 pt-5 pb-2 focus:outline-none input active:outline-none"
+              onChange={(event) => {
+                setCredentials({
+                  ...credentials,
+                  email: event.target.value,
+                });
+              }}
+              placeholder="Email or Phone"
+              type="email"
+            />
+          </div>
+
+          <div className="relative flex items-center border border-gray-500 focus:ring focus:border-orange rounded">
+            <input
+              className="w-full rounded px-3 pt-5 outline-none pb-2 focus:outline-none active:outline-none input active:border-orange"
+              onChange={(event) => {
+                setCredentials({
+                  ...credentials,
+                  password: event.target.value,
+                });
+              }}
+              placeholder="Password (6 or more characters)"
+              type="password"
+            />
+          </div>
+
+          <div className="-m-2">
+            <a
+              className="font-bold text-orange hover:underline rounded-full"
+              href="#"
+            >
+              Forgot password?
+            </a>
+          </div>
+
+          <button
+            className="w-full text-center bg-orange hover:opacity-70 rounded-full text-white py-3 font-medium"
+            onClick={signUp}
+          >
+            Agree & Join
+          </button>
+        </form>
+
+        <div className="relative flex py-5 items-center">
+          <div className="flex-grow border-t border-gray-400"></div>
+          <span className="flex-shrink mx-4 text-gray-400">Or</span>
+          <div className="flex-grow border-t border-gray-400"></div>
+        </div>
+
+        <GoogleButton onClick={signInWithGoogle} className="m-auto" />
+      </div>
+
       <p>
         Already on LinkedIn?
-        <span onClick={() => router.push("/login")}>Sign in</span>
+        <span
+          onClick={() => router.push("/sign-in")}
+          className="ml-2 text-orange cursor-pointer hover:underline"
+        >
+          Sign in
+        </span>
       </p>
     </div>
   );
--- src/components/SignInComponent.tsx
+++ src/components/SignInComponent.tsx
@@ -31,34 +31,81 @@ export default function SignInComponent() {
   };
 
   return (
-    <div>
-      <h1>Sign In Component</h1>
-      <p>Stay updated on your professional world</p>
-      <div>
-        <input
-          className="block"
-          onChange={(event) => {
-            setCredentials({ ...credentials, email: event.target.value });
-          }}
-          placeholder="Email or Phone"
-          type="email"
-        />
-        <input
-          className="block"
-          onChange={(event) => {
-            setCredentials({ ...credentials, password: event.target.value });
-          }}
-          placeholder="Password"
-          type="password"
-        />
-        <button className="block" onClick={signIn}>
-          Sign in
-        </button>
+    <div className="h-screen bg-white relative flex flex-col space-y-6 justify-center items-center">
+      <div
+        onClick={() => router.push("/")}
+        className="absolute  top-3 left-5 cursor-pointer"
+      >
+        {/* <img
+                src="https://flowbite.com/docs/images/logo.svg"
+                className="mr-3 h-6 sm:h-9"
+                alt="Flowbite Logo"
+              /> */}
+        <span className="self-center text-3xl font-semibold whitespace-nowrap text-orange dark:text-white">
+          hatake
+        </span>
       </div>
-      <GoogleButton onClick={signInWithGoogle} />
+      <div className="bg-white md:shadow-lg shadow-none rounded p-6 w-96">
+        <h1 className="text-3xl font-bold leading-normal">Sign in</h1>
+        <p className="text-sm leading-normal">
+          Stay updated on your professional world
+        </p>
+        <form className="space-y-5 mt-5">
+          <div className="mb-4 relative">
+            <input
+              className="w-full rounded px-3 border border-gray-500 pt-5 pb-2 focus:outline-none input active:outline-none"
+              onChange={(event) => {
+                setCredentials({ ...credentials, email: event.target.value });
+              }}
+              placeholder="Email or Phone"
+              type="email"
+            />
+          </div>
+          <div className="relative flex items-center border border-gray-500 focus:ring focus:border-orange rounded">
+            <input
+              className="w-full rounded px-3 pt-5 outline-none pb-2 focus:outline-none active:outline-none input active:border-orange"
+              onChange={(event) => {
+                setCredentials({
+                  ...credentials,
+                  password: event.target.value,
+                });
+              }}
+              placeholder="Password"
+              type="password"
+            />
+          </div>
+          <div className="-m-2">
+            <a
+              className="font-bold text-orange hover:underline rounded-full"
+              href="#"
+            >
+              Forgot password?
+            </a>
+          </div>
+          <button
+            className="w-full text-center bg-orange hover:opacity-70 rounded-full text-white py-3 font-medium"
+            onClick={signIn}
+          >
+            Sign in
+          </button>
+        </form>
+
+        <div className="relative flex py-5 items-center">
+          <div className="flex-grow border-t border-gray-400"></div>
+          <span className="flex-shrink mx-4 text-gray-400">Or</span>
+          <div className="flex-grow border-t border-gray-400"></div>
+        </div>
+
+        <GoogleButton onClick={signInWithGoogle} className="m-auto" />
+      </div>
       <p>
         New to LinkedIn?
-        <span onClick={() => router.push("/register")}>Join now</span>
+        <span
+          onClick={() => router.push("/register")}
+          className="ml-2 text-orange cursor-pointer hover:underline"
+        >
+          Join now
+        </span>
       </p>
     </div>
   );
